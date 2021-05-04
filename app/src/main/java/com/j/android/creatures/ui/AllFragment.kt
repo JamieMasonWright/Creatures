404f95831b7a39bed41799d2adff99780e133a01
Jamie Mason_Wright <jamie.mason-wright@lush.co.uk>
2021-05-04 16:10:47 +0100

    Added Basic Layout options using changing span sizes

--- app/src/main/java/com/j/android/creatures/ui/AllFragment.kt
+++ app/src/main/java/com/j/android/creatures/ui/AllFragment.kt
@@ -1,12 +1,11 @@
 package com.j.android.creatures.ui
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.j.android.creatures.R
 import com.j.android.creatures.model.CreatureStore
 import kotlinx.android.synthetic.main.fragment_all.*
@@ -15,11 +14,24 @@ import kotlinx.android.synthetic.main.fragment_all.*
 class AllFragment : Fragment() {
 
   private val adapter = CreatureCardAdapter(CreatureStore.getCreatures().toMutableList())
+  private lateinit var layoutManager: StaggeredGridLayoutManager
 
   companion object {
     fun newInstance(): AllFragment {
       return AllFragment()
     }
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?)
+  {
+    super.onCreate(savedInstanceState)
+    setHasOptionsMenu(true)
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater)
+  {
+    super.onCreateOptionsMenu(menu, inflater)
+    inflater.inflate(R.menu.menu_all, menu)
   }
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -28,9 +40,32 @@ class AllFragment : Fragment() {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    creature_recycler_view.layoutManager = GridLayoutManager(activity, 2, GridLayoutManager.VERTICAL, false)
-
+    layoutManager = StaggeredGridLayoutManager(2,  GridLayoutManager.VERTICAL)
+    creature_recycler_view.layoutManager = layoutManager
     creature_recycler_view.adapter = adapter
+  }
 
+  override fun onOptionsItemSelected(item: MenuItem): Boolean
+  {
+    val id = item.itemId
+    when(id){
+      R.id.accessibility_action_clickable_span_1 -> {
+        showListView()
+        return true
+        }
+      R.id.accessibility_action_clickable_span_2 -> {
+        showGridView()
+        return true
+      }
+    }
+    return super.onOptionsItemSelected(item)
+  }
+
+  private fun showListView(){
+    layoutManager.spanCount = 1
+  }
+
+  private fun showGridView(){
+    layoutManager.spanCount = 1
   }
 }
--- app/src/main/java/com/j/android/creatures/ui/CreatureCardAdapter.kt
+++ app/src/main/java/com/j/android/creatures/ui/CreatureCardAdapter.kt
@@ -1,6 +1,5 @@
 package com.j.android.creatures.ui
 
-import android.content.ContentQueryMap
 import android.content.Context
 import android.graphics.BitmapFactory
 import android.graphics.Color
@@ -12,33 +11,27 @@ import androidx.recyclerview.widget.RecyclerView
 import com.j.android.creatures.R
 import com.j.android.creatures.app.inflate
 import com.j.android.creatures.model.Creature
-import kotlinx.android.synthetic.main.list_item_creature.view.*
-import kotlinx.android.synthetic.main.list_item_creature.view.creature_image
-import kotlinx.android.synthetic.main.list_item_creature.view.nickname
 import kotlinx.android.synthetic.main.list_item_creature_card.view.*
 
-class CreatureCardAdapter(private val creatures: MutableList<Creature>) : RecyclerView.Adapter<CreatureCardAdapter.ViewHolder>()
-{
+class CreatureCardAdapter(private val creatures: MutableList<Creature>): RecyclerView.Adapter<CreatureCardAdapter.ViewHolder>() {
 
-	class ViewHolder(itemView: View) : View.OnClickListener, RecyclerView.ViewHolder(itemView){
-		private lateinit var creature : Creature
+	class ViewHolder(itemView: View) : View.OnClickListener, RecyclerView.ViewHolder(itemView) {
+		private lateinit var creature: Creature
 
-		fun bind(creature: Creature){
+		init {
+			itemView.setOnClickListener(this)
+		}
+
+		fun bind(creature: Creature) {
 			this.creature = creature
 			val context = itemView.context
 			val imageResource = context.resources.getIdentifier(creature.uri, null, context.packageName)
 			itemView.creature_image.setImageResource(imageResource)
-			itemView.nickname.text = creature.nickname
-			setBackgroundColour(context, imageResource)
+			itemView.full_name.text = creature.fullName
+			setBackgroundColors(context, imageResource)
 		}
 
-		init
-		{
-			itemView.setOnClickListener(this)
-		}
-
-		override fun onClick(view: View?)
-		{
+		override fun onClick(view: View?) {
 			view?.let {
 				val context = it.context
 				val intent = CreatureActivity.newIntent(context, creature.id)
@@ -46,38 +39,37 @@ class CreatureCardAdapter(private val creatures: MutableList<Creature>) : Recycl
 			}
 		}
 
-		private fun setBackgroundColour(context: Context, imageResource: Int){
+		private fun setBackgroundColors(context: Context, imageResource: Int) {
 			val image = BitmapFactory.decodeResource(context.resources, imageResource)
-			Palette.from(image).generate{ palette ->
+			Palette.from(image).generate { palette ->
 				palette?.let {
-					val backgroundColor = it.getDominantColor(ContextCompat.getColor(context, R.color.design_default_color_primary_dark))
+					val backgroundColor = it.getDominantColor(ContextCompat.getColor(context, R.color.colorPrimaryDark))
 					itemView.creature_card.setBackgroundColor(backgroundColor)
-					itemView.nick_name_holder.setBackgroundColor(backgroundColor)
-					val textColour = if(isColourDark(backgroundColor)) Color.WHITE else Color.BLACK
-					itemView.nickname.setTextColor(textColour)
-
-
+					itemView.name_holder.setBackgroundColor(backgroundColor)
+					val textColor = if (isColorDark(backgroundColor)) Color.WHITE else Color.BLACK
+					itemView.full_name.setTextColor(textColor)
 				}
 			}
 		}
-		private fun isColourDark(color: Int): Boolean
-		{
-			val darkness = 1 - (0.299 * Color.red(color)) + 0.587 * Color.green(color) + 0.114 * Color.blue(color) / 25
+
+		private fun isColorDark(color: Int): Boolean {
+			val darkness = 1 - (0.299 * Color.red(color) +
+					0.587 * Color.green(color) +
+					0.114 * Color.blue(color)) / 255
 			return darkness >= 0.5
 		}
+
 	}
 
-
-	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
-	{
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 		return ViewHolder(parent.inflate(R.layout.list_item_creature_card))
 	}
 
-	override fun onBindViewHolder(holder: ViewHolder, position: Int)
-	{
+	override fun getItemCount() = creatures.size
+
+	override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 		holder.bind(creatures[position])
 	}
 
-	override fun getItemCount(): Int = creatures.size
 
 }
